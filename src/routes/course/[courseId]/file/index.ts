37f80cc0e61485a8e18270ce60bb766e0e3505c6
cocomine <cocomine@cocomine.cc>
2026-03-23 19:35:17 +0800

    refactor course file API: enhance file upload handling with content type validation, database integration, and error management

--- src/routes/course/[courseId]/file/index.ts
+++ src/routes/course/[courseId]/file/index.ts
@@ -2,6 +2,10 @@ import {ErrorRequestHandler, Response, Router} from "express";
 import {getLogger} from "log4js";
 import {CourseRequest} from "../index";
 import multer from "multer";
+import {fileTypeFromBuffer} from "file-type";
+import {DB} from "../../../../sql_service";
+import {createHash} from "node:crypto";
+import xss from "xss";
 
 const router = Router({mergeParams: true});
 const logger = getLogger("/course/[courseId]/file");
@@ -53,15 +57,46 @@ router.get("/", async (req: CourseRequest, res) => {
 // path: /course/[courseId]/file/
 // POST: upload file
 router.post("/", [upload.single("file"), errorHandler], async (req: CourseRequest, res: Response) => {
+    const {courseId} = req.params;
+
     if (req.header("content-type")?.includes("multipart/form-data") === false) {
         return res.status(400).json({code: 400, message: "Content-Type must be multipart/form-data"});
     }
-    if (!req.file) {
+
+    const file = req.file;
+    if (!file) {
         return res.status(400).json({code: 400, message: "No file uploaded"});
     }
 
-    //todo: save database
-    res.status(200).json([req.file.size, req.file.filename, req.file.mimetype]);
+    const trueMimeType = await fileTypeFromBuffer(file.buffer);
+    if (trueMimeType) {
+        if (trueMimeType.mime.includes("video/")) {
+            return res.status(400).json({code: 400, message: "Does not support video!"});
+        } else if (trueMimeType.mime.includes("audio/")) {
+            return res.status(400).json({code: 400, message: "Does not support audio!"});
+        }
+    }
+
+    // save database
+    const sha256 = createHash("sha256").update(file.buffer).digest("hex");
+    const fileId = crypto.randomUUID();
+    await DB.exec("BEGIN");
+    try {
+        await DB.run("INSERT OR IGNORE INTO file_blob (sha256, blob, size, mime) VALUES (?, ?, ?, ?)", [sha256, file.buffer, file.size, trueMimeType?.mime ?? file.mimetype ?? "application/octet-stream"]);
+        await DB.run("INSERT INTO files (ID, course_id, filename, sha256) VALUES (?, ?, ?, ?)", [fileId, courseId, xss(file.originalname), sha256]);
+        await DB.exec("COMMIT");
+    } catch (err) {
+        await DB.exec("ROLLBACK");
+        logger.error(err);
+
+        return res.status(500).json({code: 500, message: "Failed to save file to database"});
+    }
+
+    res.status(200).json({
+        code: 200,
+        message: "File uploaded successfully",
+        data: {fileId, mime: trueMimeType?.mime ?? file.mimetype ?? "application/octet-stream"}
+    });
 });
 
 // path: /course/[courseId]/file/[fileId]/*
